01ca4230d6f344e068181ac69efa5f2610947154
HaroonAzhar <haroon1azhar@gmail.com>
2026-08-07 20:13:08 -0400

    solved 994

--- leetcode/994/994.py
+++ leetcode/994/994.py
@@ -1,1 +1,29 @@
 # 994. Rotting Oranges
+from typing import List
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        fresh, time = 0, 0
+        m, n = len(grid), len(grid[0])
+        q = deque()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh +=1
+                elif grid[i][j] ==2:
+                    q.append((i, j))
+        dirs = [[1, 0],[-1, 0],[0, 1], [0, -1]]
+        while q and fresh:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr,dc in dirs:
+                    nr,nc = r+dr,c+dc
+                    if 0<=nr<m and 0<=nc<n and grid[nr][nc]==1:
+                        grid[nr][nc]=2
+                        q.append((nr,nc))
+                        fresh-=1
+
+            time+=1
+
+        return time if fresh ==0 else -1
