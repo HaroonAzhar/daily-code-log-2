55665eca684a1affdd723e1060a9be0a78c4bfbf
HaroonAzhar <haroon1azhar@gmail.com>
2026-09-18 02:00:28 -0400

    solved 238

---
+++ leetcode/Round2/334/334.py
@@ -0,0 +1,12 @@
+# 334. Increasing Triplet Subsequence
+def increasingTriplet(self, nums):
+    first = float('inf')
+    second = float('inf')
+    for n in nums:
+        if n <= first:
+            first = n
+        elif n <= second:
+            second = n
+        else:
+            return True
+    return False
