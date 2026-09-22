924a960d4024457001a84ccf105ef16757ba54f5
HaroonAzhar <haroon1azhar@gmail.com>
2026-09-21 22:16:47 -0400

    solved 2462

---
+++ leetcode/Round2/2462/2462.py
@@ -0,0 +1,27 @@
+# 2462. Total Cost to Hire K Workers
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        # FASTER AND EASIER WAY
+        if 2 * candidates + k > len(costs):
+            costs.sort()
+            return sum(costs[:k])
+                
+        left = candidates
+        right = len(costs) - candidates - 1
+
+        heap_left = costs[:left]
+        heap_right = costs[right + 1:]
+
+        heapq.heapify(heap_left)
+        heapq.heapify(heap_right)
+
+        total = 0
+        for _ in range(k):
+            if heap_left[0] <= heap_right[0]:
+                total += heapq.heapreplace(heap_left, costs[left])
+                left += 1
+            else:
+                total += heapq.heapreplace(heap_right, costs[right])
+                right -= 1
+
+        return total
