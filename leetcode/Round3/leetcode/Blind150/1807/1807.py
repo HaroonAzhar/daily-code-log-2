9c6c213ca84d85de0ee1f8232834dd2b917918df
HaroonAzhar <haroon1azhar@gmail.com>
2026-09-25 23:46:32 -0400

    solved 1807

---
+++ leetcode/Round3/leetcode/Blind150/1807/1807.py
@@ -0,0 +1,5 @@
+# 1807. Evaluate the Bracket Pairs of a String
+class Solution:
+    def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
+        d = dict(knowledge)
+        return re.sub(r"\((\w+)\)", lambda m: d.get(m[1], "?"), s)
